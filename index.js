7bd9841b1d5783797ac090750a8cc09b9e2f9cbf
Ashar Riaz <97333911+Asharriaz14@users.noreply.github.com>
2024-10-01 17:18:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000; // Use Vercel's PORT or default to 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
