7f528266c30513e8c76c6870203a41a0b5d0b333
Ashar Riaz <97333911+Asharriaz14@users.noreply.github.com>
2024-10-01 17:33:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import express from 'express';
 const app = express();
 
 // Simple test route to check if the API is working
-app.get('/test', (req, res) => {
-    res.status(200).json({ message: "Vercel is working!" });
+app.get('/current', (req, res) => {
+    // Get the current date and time
+    const now = new Date();
+    // Return the current date and time as a JSON response
+    res.json({ current: now.toISOString() });
 });
-
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
